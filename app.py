b1b05d52c4194ccd598d377cf2e39d056c1f6521
Alex-Gaming-Studios <alexgaming.padh@icloud.com>
2025-04-20 10:32:26 +0300

    app.py aktualisieren

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')  # Lade die index.html Seite
+    return "OfficeLight läuft!"
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))  # Render benötigt Port von Umgebungsvariablen
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run()
